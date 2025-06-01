a32b31edf40cfc9c6d8a4fe70f4b17457a2d4f08
Muhammad Farhan <142079152+muhammadfarhan024260@users.noreply.github.com>
2025-06-01 09:35:23 +0500

    Update GeminiApi.java
    API Key changed

--- GeminiApi.java
+++ GeminiApi.java
@@ -8,7 +8,7 @@ import org.json.JSONObject;
 
 public class GeminiApi {
 
-    private static final String API_KEY = "AIzaSyBG-KoePXLaB5HALyzSrIEPcjJYHcovpLM"; // Replace with your API key
+    private static final String API_KEY = "API Key"; // Replace with your API key
     private static final String API_URL
             = "https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=" + API_KEY;
 
